1c94c05aca318e827f7a7f3a774014354e74fdb8
jeffry Martinez02 <121065170+jeffry02@users.noreply.github.com>
2023-05-06 21:12:44 -0600

    Add files via upload

---
+++ calculator 1.5/js/calculator.js
@@ -0,0 +1,19 @@
+export class Calculator {
+  constructor (){} 
+  
+  sum(num1, num2) {
+    return num1 + num2;
+  }
+
+  subtract(num1, num2) {
+    return num1 - num2;
+  }
+
+  divide(num1, num2) {
+    return num1 / num2;
+  }
+
+  multiply(num1, num2) {
+    return num1 * num2;
+  }
+}
